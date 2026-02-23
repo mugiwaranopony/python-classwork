87607c097d23ca50c70e95fa70d9678ad8da1109
mugiwaranopony <mariam.lashkhia1@gmail.com>
2026-02-23 17:39:55 +0000

    grade.py

---
+++ conditionals/compare.py
@@ -0,0 +1,38 @@
+# x = int(input("What's x?"))
+# y = int(input("What's y?"))
+
+# if x < y:
+#     print("x is less than y")
+# if x > y:
+#     print("x is greater than y")
+# if x == y:
+#     print("x equals y")
+
+# if x < y:
+#     print("x is less than y")
+# elif x > y:
+#     print("x is greater than y")
+# elif x == y:
+#     print("x equals y")
+
+
+
+# if x < y:
+#     print("x is less than y")
+# elif x > y:
+#     print("x is greater than y")
+# else:
+#     print("x equals y")
+
+x = int(input("What's x?"))
+y = int(input("What's y?"))
+
+
+# if x < y or x > y:
+#     print("x does not equal y")
+# if x == y:
+#     print("x does not equal y")
+if x != y:
+    print("x does not equal y")
+else:
+    print("x equals y")
