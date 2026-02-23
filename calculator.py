f524729a02f3f8bdd74d3fbfebfd1b745f3153bd
mugiwaranopony <mariam.lashkhia1@gmail.com>
2026-02-23 16:26:58 +0000

    hello.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,16 @@
-x = float(input("What's x? "))
-y = float(input("What's y? "))
+# x = float(input("What's x? "))
+# y = float(input("What's y? "))
 
-z = round(x / y, 2)
-print(z)
+# z = round(x / y, 2)
+# print(z)
+
+
+def main():
+    x = int(input("What's x? "))
+    print("x square is", square(x))
+
+def square(n):
+    return n * n
+
+
+main()
---
+++ hello.py
@@ -0,0 +1,8 @@
+def hello(to):
+    print("hello", to)
+
+name = input("What's your name? ").strip().title()
+
+hello(name)
+# hello()
+# print(f"Hello, {name}")
