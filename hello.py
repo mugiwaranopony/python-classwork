b4bbd153cd4885bb83fae905eafddd3a61bded99
mugiwaranopony <mariam.lashkhia1@gmail.com>
2026-03-09 17:32:19 +0000

    hogwarts

--- hello.py
+++
@@ -1,8 +0,0 @@
-def hello(to):
-    print("hello", to)
-
-name = input("What's your name? ").strip().title()
-
-hello(name)
-# hello()
-# print(f"Hello, {name}")
---
+++ loops/hogwarts.py
@@ -0,0 +1,9 @@
+students = {
+    "Hermione": "Griffyndor",
+    "Harry": "Griffyndor",
+    "Ron": "Griffyndor"
+    "Draco": "Slytherin",
+}
+
+for student in students:
+    print(student, students[student], sep=", ")
